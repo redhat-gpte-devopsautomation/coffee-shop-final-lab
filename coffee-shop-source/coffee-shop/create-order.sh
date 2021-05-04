ed03fa45a46e67124fbfdf0d00dcd0920ba17469
Wolfgang Kulhanek <WolfgangKulhanek@gmail.com>
2021-05-04 14:55:35 -0400

    Remove app source code

--- coffee-shop-source/coffee-shop/create-order.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-set -eu
-
-curl http://$(oc get route coffee-shop --template='{{ .spec.host }}'/orders -XPOST -i -H 'Content-Type: application/json' -d '{"origin":"Colombia","type":"Espresso"}'
